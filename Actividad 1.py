daf185cee858fb6da635193604d729df2590d85c
carlosjhoan <43355110+carlosjhoan@users.noreply.github.com>
2023-10-04 18:07:40 -0500

    Add files via upload

---
+++ Actividad 1.py
@@ -0,0 +1,28 @@
+
+kilometros = 800 
+
+dias = 7
+
+preciodelkilometro = 0.63
+
+descuento = 30
+
+
+kilometros = int(input("ingrese los kilometros que recorrera: "))
+dias = int(input("ingrese los dias que se quedara: "))
+
+#precio sin el descuento
+if kilometros > 800 and dias > 7:
+    preciosindescuento = kilometros * preciodelkilometro
+
+    preciodeldescuento = preciosindescuento - descuento
+
+else : 
+    preciosindescuento = kilometros * preciodelkilometro
+    print("el precio final seria: ${:,.2f}".format(preciosindescuento))
+    
+  
+
+
+
+
